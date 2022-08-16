b323120d887a992e3e7c67c0d94f91065ad3cfbb
MalinkyZubr <96273195+MalinkyZubr@users.noreply.github.com>
2022-08-16 09:26:54 -0700

    e

---
+++ Learning/Unit-1-primitive-types/Printing/Timeline.java
@@ -0,0 +1,16 @@
+public class Timeline {
+    
+}
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age | M/YY - Description");
+        System.out.println("6   | 5/11 - Moved from Canada to US\n    |\n    |\n    |\n    |\n");
+        System.out.println("8   | 8/12 - Started 2nd Grade\n    |\n    |\n    |\n    |\n");
+        System.out.println("10  | 1/15 - Made my first radio\n    |\n    |\n    |\n    |\n");
+        System.out.println("12  | 8/19 - Wrote 'Hello World' in HTML\n    |\n    |\n    |\n    |\n");
+        System.out.println("14  | 12/20 - First course in Python\n    |\n    |\n    |\n    |\n");
+        System.out.println("16  | 5/22 - Hacked my first computer\n    |\n    |\n    |\n    |\n");
+    }
+}
---
+++ Learning/Unit-1-primitive-types/Variables/Variables.java
@@ -0,0 +1,22 @@
+public class Variables 
+{
+
+    public static void main(String[] args)
+    {
+        int snapchatsSent = 353;
+        System.out.print(String.format("Number of snapchats sent: %d\n", snapchatsSent));
+        
+        double youTubeVideosWatched = 130.5;
+        System.out.print(String.format("Number of YouTube videos watched: %f\n", youTubeVideosWatched));
+        
+        String favoriteApp = "Instagram";
+        System.out.print(String.format("Favorite app: %s\n", favoriteApp));
+
+        youTubeVideosWatched = 240.4;
+        System.out.print(String.format("Number of YouTube videos watched: %f\n", youTubeVideosWatched));
+        
+        char firstLetter = 'A';
+        System.out.print(String.format("First letter: %c\n", firstLetter));
+    }
+
+}
