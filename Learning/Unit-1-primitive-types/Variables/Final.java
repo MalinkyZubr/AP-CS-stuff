f54df13b7f497d0b0f6ee036ef3bcbf201adcff8
MalinkyZubr <96273195+MalinkyZubr@users.noreply.github.com>
2022-08-17 12:17:13 -0700

    fix bug

--- Learning/Unit-1-primitive-types/Variables/Final.java
+++ Learning/Unit-1-primitive-types/Variables/Final.java
@@ -6,7 +6,7 @@ public class Final
         final int wontChange = 10;
         //When we try to change the value of wontChange
         //it throws an error!
-        wontChange = 5;
+        // wontChange = 5;
         //delete line 9 and the program will work!
         System.out.println(wontChange);
     }
